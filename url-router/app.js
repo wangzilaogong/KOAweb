2b89c60ee61c1df3328f36aa3d076bce79186a65
wangzilaogong <997972547@qq.com>
2016-11-13 18:08:11 +0800

    url2-koa

---
+++ url-router/app.js
@@ -0,0 +1,43 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+const router = require('koa-router')();//返回的是函数
+const bodyParser = require('koa-bodyparser');//解析body的功能
+
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+
+
+app.use(async (ctx, next) => {
+    console.log('Process ${ctx.request.method} ${ctx.request.url}...');
+    await next();
+});
+
+app.use(bodyParser());
+//add url-route
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+    
+});
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+//add router middleware
+app.use(router.routes());
+
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
